da314aa42c47f1586bccab1fe0c17caa2ef5ec34
shusharin <v.v.shusharin@gmail.com>
2013-10-14 01:22:33 +0400

    first

---
+++ admin/language/russian/extension/news.php
@@ -0,0 +1,18 @@
+﻿<?php
+// Heading 
+$_['heading_title']     = 'Новости';
+
+// Text
+$_['text_title']		= 'Заголовок';
+$_['text_description']	= 'Описание';
+$_['text_date']			= 'Дата добавления';
+$_['text_action']		= 'Action';
+$_['text_status']		= 'Статус';
+$_['text_keyword']		= 'SEO Keyword';
+
+// Success
+$_['text_success']		= 'Вы успешно изменили новость!';
+
+// Error
+$_['error_permission'] = 'Внимание: Вы не имеете права изменять новость!';
+?>
