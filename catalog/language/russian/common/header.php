867dfdff9a44f3720da418121af1c0c053b57f53
shusharin <v.v.shusharin@gmail.com>
2013-10-14 02:22:39 +0400

    add head_bottom

--- catalog/language/russian/common/header.php
+++ catalog/language/russian/common/header.php
@@ -1,6 +1,8 @@
 <?php
 // Text
 $_['text_home']     = 'Главная';
+$_['text_wishlist']       = 'Список пожеланий (%s)';
+$_['text_shopping_cart']  = 'Корзина';
 $_['text_special']  = 'Акции';
 $_['text_contact']  = 'Контакты';
 $_['text_sitemap']  = 'Карта сайта';
